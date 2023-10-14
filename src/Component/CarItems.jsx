a854858521bfdee84541d345e391fa918dc6f046
Anjali <anjalinegian2002@gmail.com>
2023-10-14 21:18:02 +0530

    add image in source file

--- src/Component/CarItems.jsx
+++ src/Component/CarItems.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import CarCard from './CarCard';
 
-
 const CarItems = () => {
     const resultdisplay = useSelector((state)=> state.FindCar.displayCarItems)
     
